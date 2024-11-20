4038d7c913d7e688f3e3ac6cfe56ca8be750f837
vluu11 <luuvuong11@gmail.com>
2024-11-19 16:18:29 -0800

    added server code

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,14 @@ import express from 'express';
 // import path from 'node:path';
 import db from './config/connection.js';
 import routes from './routes/index.js';
+import path from 'node:path';
+
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+// 👇️ "/home/john/Desktop/javascript"
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serves static files in the entire client's dist folder
-app.use(express.static('../client/dist'));
+app.use(express.static('../../client/dist'));
 
 app.use(routes);
 
