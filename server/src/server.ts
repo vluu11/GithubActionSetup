b565655609c98075eae2b26418f66f2b86307ced
vluu11 <luuvuong11@gmail.com>
2024-11-19 16:24:32 -0800

    added server code

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import type { Request, Response } from 'express';
 // import path from 'node:path';
 import db from './config/connection.js';
 import routes from './routes/index.js';
@@ -18,7 +19,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serves static files in the entire client's dist folder
-app.use(express.static('../../client/dist'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../../client/dist')));
+
+  app.get('*', (_req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+  });
+}
 
 app.use(routes);
 
